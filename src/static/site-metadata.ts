2e9e81f7daad1f2ef99fe8c4833fd93edb68f73e
Rollin Luo <rowlinerollin@gmail.com>
2024-01-13 16:10:04 +0800

    Update site-metadata.ts
    change my info

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,15 +11,15 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'Rollin Workouts Map',
+  siteUrl: 'https://www.rollin.love',
+  logo: 'http://www.rollin.love/wp-content/uploads/2023/04/running_page.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'http://rollin.love',
     },
     {
       name: 'About',
