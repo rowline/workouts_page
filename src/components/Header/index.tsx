2617d8b65664aa8b5c7260f55f28e86feb88c90b
Rollin Luo <rollin@Rollins-MacBook-Air-2.local>
2026-02-11 13:37:42 +0800

    fix: header layout alignment and restore search function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -70,17 +70,30 @@ const Header = () => {
             <button
               id="search-button"
               aria-label="Search"
-              className="text-base hover:text-primary-600 dark:hover:text-primary-400 transition-colors ml-5"
+              className="text-base hover:text-primary-600 dark:hover:text-primary-400 transition-colors"
+              onClick={() => (window.location.href = '/search/')}
             >
               <span className="relative block icon">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16 h-5 w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128-128 128z"></path>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="search"
+                  className="svg-inline--fa fa-search fa-w-16 h-5 w-5"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128-128 128z"
+                  ></path>
                 </svg>
               </span>
             </button>
 
             {/* Dark Mode Switcher */}
-            <div className="ltr:mr-14 rtl:ml-14 flex items-center ml-5">
+            <div className="ltr:mr-14 rtl:ml-14 flex items-center">
               <button
                 onClick={toggleTheme}
                 aria-label="Dark mode switcher"
